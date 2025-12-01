2c91c8d82124698342fcf78d888a6ba962cf745f
rimpa kasundi <rimpakasundi@gmail.com>
2025-12-01 21:53:43 +0530

    Update Navbar with responsive design and Bengali text

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,29 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import RootLayout from "./layouts/RootLayout";
+import Home from "./components/Home/Home";
 
-createRoot(document.getElementById("root")).render(<StrictMode></StrictMode>);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "",
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
